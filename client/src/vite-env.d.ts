7e9381d19917b6688cd4da604c92d329401f42b1
hemanthkunta <hemanthkuntanani999@gmail.com>
2026-09-01 14:47:18 +0530

    sync: automatic checkpoint 2026-09-01 09:17:18 UTC

---
+++ client/src/vite-env.d.ts
@@ -0,0 +1,13 @@
+﻿/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL?: string;
+  readonly VITE_STUN_URLS?: string;
+  readonly VITE_TURN_URLS?: string;
+  readonly VITE_TURN_USERNAME?: string;
+  readonly VITE_TURN_PASSWORD?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
